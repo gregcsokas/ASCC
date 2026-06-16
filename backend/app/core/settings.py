c93f50f591c8c8f716012c961be4811140345f1a
Gergő Csókás <gergo.csokas@gmail.com>
2026-06-16 15:51:27 +0200

    :wrench: [Backend] Add app settings and related distributable `.env` file

---
+++ backend/app/core/settings.py
@@ -0,0 +1,21 @@
+import os
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    """Application settings."""
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    # Database related settings
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_PORT: int = os.getenv("DB_PORT")
+    DB_NAME: str = os.getenv("DB_NAME")
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+
+    # Logging related settings
+    LOG_LEVEL: str = os.getenv("LOG_LEVEL", "DEBUG")
+
+settings = Settings()
+    
