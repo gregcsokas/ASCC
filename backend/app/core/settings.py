87ddd5e7c533710bf9dced16c9ed0d115b726f20
Gergő Csókás <gergo.csokas@gmail.com>
2026-06-17 09:31:09 +0200

    :recycle: [Backend] Remove antipattern `os.getenv` usage from `settings.py`

--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -8,14 +8,13 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     # Database related settings
-    DB_HOST: str = os.getenv("DB_HOST")
-    DB_PORT: int = os.getenv("DB_PORT")
-    DB_NAME: str = os.getenv("DB_NAME")
-    DB_USER: str = os.getenv("DB_USER")
-    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5432
+    DB_NAME: str = "ascc"
+    DB_USER: str = "ascc"
+    DB_PASSWORD: str = "ascc_root"
 
     # Logging related settings
-    LOG_LEVEL: str = os.getenv("LOG_LEVEL", "DEBUG")
+    LOG_LEVEL: str = "DEBUG"
 
 settings = Settings()
-    
